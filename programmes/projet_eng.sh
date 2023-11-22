3871ee61c77362a01263357547ae332f60f6c069
Clara Sachot <raclax@Ordi.tout.court>
2023-11-22 17:30:30 +0100

    fin prijet1 clara

--- programmes/projet_eng.sh
+++ programmes/projet_eng.sh
@@ -6,10 +6,8 @@ then
 	echo "Ce script demande un argument"
 	exit
 else
-	if [ -f "$URL" ]
+	if ! [ -f "$URL" ]
 	then 
-		echo "Le fichier existe bien"
-	else
 		echo "Le fichier existe pas" 
 		exit
 	fi
@@ -23,13 +21,14 @@ echo "<html>
 	<body>"
 
 echo "		<table>
-		<tr><th>Numéro</th><th>URL</th><th>codeHTTP</th><th>encodage</th><th>aspiration</th><dump</th></tr>"
+		<tr><th>Numéro</th><th>URL</th><th>Code HTTP</th><th>Encodage</th><th>Aspiration</th><Dump</th><th>Nonbre d'occurences</th><th>Contexte</th></tr>"
 while read -r URL;
 do
 	response=$(curl -s -I -L -w "%{http_code}" -o "../aspirations/aspiration_ang$N.html" $URL)
 	CODE=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
        lynx -dump $URL >../dumps-text/dump_ang$N.html
-
+	COMPTE=$(cat ../dumps-text/dump_ang$N.html | egrep -i -o " link(s)? "|wc -w) 
+	cat ../dumps-text/dump_ang$N.html | egrep -2  " link(s)? " > ../contextes/contexte_ang$N.txt
 	echo "<tr>
 		<td>"$N"</td>
 		<td>"$URL"</td>
@@ -37,6 +36,8 @@ do
 		<td>"$CODE"</td>
 		<td><a href="../aspirations/aspiration_ang$N.html">aspiration</a></td>
 		<td><a href="../dumps-text/dump_ang$N.html">dump</a></td>
+		<td>"$COMPTE"</td>
+		<td><a href="../contextes/contexte_ang$N.txt">contexte</a></td>
 	</tr>"	
 	  N=$(expr $N + 1)
 
