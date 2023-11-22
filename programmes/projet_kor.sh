393f45c220dd395ccf9d3a75db1287543dc3249f
Débora <vdzdebora@gmail.com>
2023-11-22 11:57:28 +0100

    Modifications coréen

--- programmes/projet_kor.sh
+++ programmes/projet_kor.sh
@@ -1,5 +1,4 @@
 DOC=$1
-N=1
 
 if [ $# -ne 1 ]
 then
@@ -9,9 +8,9 @@ Usage : $0 ~/Desktop/Cours/ppe/PPE_lien/URLs/liens_gwangye.txt"
 else
 	if [ -f "$DOC" ]
 	then 
-		echo "Le fichier existe bien"
+		echo "Le fichier existe bien."
 	else
-		echo "Le fichier n'existe pas
+		echo "Le fichier n'existe pas !
 Usage : $0 ~/Desktop/Cours/ppe/PPE_lien/URLs/liens_gwangye.txt" 
 		exit
 	fi
@@ -26,9 +25,12 @@ echo "<html>
 
 echo "		<table>
 		<tr><th>Numéro</th><th>URL</th><th>codeHTTP</th><th>encodage</th></tr>"
+
+N=1
 while read -r URL;
 do
-	response=$(curl -s -I -L -w "%{http_code}" -o /dev/null $URL)
+	langue=$(basename $URL .txt)
+	response=$(curl -s -I -L -w "%{http_code}" -o "./aspirations/${langue}${N}.html" $URL)
 	CODE=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
 	echo "<tr>
 		<td>"$N"</td>
