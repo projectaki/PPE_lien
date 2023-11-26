3b77442629d01677a429b5274c52b4a1a009f754
Débora <vdzdebora@gmail.com>
2023-11-26 23:46:06 +0100

    Modifications coréen

--- programmes/projet_kor.sh
+++ programmes/projet_kor.sh
@@ -1,4 +1,4 @@
-DOC=$1
+urls=$1
 
 if [ $# -ne 1 ]
 then
@@ -6,7 +6,7 @@ then
 Usage : $0 ~/Desktop/Cours/ppe/PPE_lien/URLs/liens_gwangye.txt"
 	exit
 else
-	if [ -f "$DOC" ]
+	if [ -f "$urls" ]
 	then 
 		echo "Le fichier existe bien."
 	else
@@ -24,25 +24,26 @@ echo "<html>
 	<body>"
 
 echo "		<table>
-		<tr><th>Numéro</th><th>URL</th><th>codeHTTP</th><th>encodage</th></tr>"
+		<tr><th>Numéro</th><th>urls</th><thcode HTTP</th><th>Encodage</th></tr><th>Aspiration</th><Dump</th><th>Nombre d'occurences</th><th>Contexte</th></tr>"
 
-N=1
-while read -r URL;
+N=0
+while read -r urls;
 do
-	langue=$(basename $URL .txt)
-	response=$(curl -s -I -L -w "%{http_code}" -o "./aspirations/${langue}${N}.html" $URL)
-	CODE=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
-	echo "<tr>
+	response=$(curl -s -I -L -w "%{httpcode}" -o "./aspirations/aspiration_kor${N}.html" $urls)
+code=$(curl -s -I -L -w "%{content_type}" -o /dev/null $urls | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
+	curl $urls >../aspirations/aspiration_kor$N.txt
+	lynx -dump $urls >../dumps-text/dump_kor$N.txt
+	compte=$(cat ./dumps-text/dump_kor${N}.html | grep -i -o -E "(관계|링크|끈)"  | wc -w)
+    contexte=$(cat ./dumps-text/dump_kor${N}.html | grep -B 1 -A 1 -i -w -E "(관계|링크|끈)" > "./contexte/contexte_kor${N}.txt" )
+	N=$(expr $N + 1)
+		echo "<tr>
 		<td>"$N"</td>
-		<td>"$URL"</td>
+		<td>"$urls"</td>
 		<td>"$response"</td>
-		<td>"$CODE"</td>
+		<td>"$code"</td>
 	</tr>"
-	curl $URL >../aspirations/aspiration_kor$N.txt
-	lynx -dump $URL >../dumps-text/dump_kor$N.txt
-	  N=$(expr $N + 1)
 
-done < "$DOC"
+done < "$urls"
 echo "		</table>
 	</body>
 </html>" 
