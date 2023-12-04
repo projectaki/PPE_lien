e1fe22dc0033eb6258b85a8451a0da6a90e7048e
mariahul1 <hul.maria88@gmail.com>
2023-12-04 20:06:19 +0100

    Update script

--- itrameur/make_itrameur_corpus_1.sh
+++ itrameur/make_itrameur_corpus_1.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "On a besoin de 2 arguments : dossier dumps-txt avec son chemin et la LANGue"
+if [ "$#" -ne 3 ]; then
+    echo "On a besoin de 3 arguments : dossier dumps-txt avec son chemin, dossier contextes avec son chemin et la langue"
     exit 1
 fi
 
 DOSSIER_DUMPS=$1
-LANG=$2
+DOSSIER_CONTEXTES=$2
+LANG=$3
 
 output_dump="Desktop/PPE_lien/itrameur/dumps-text_${LANG}.txt"
 echo "<LANG=\"${LANG}\">" > "$output_dump"
 
+output_contextes="Desktop/PPE_lien/itrameur/contextes_${LANG}.txt"
+echo "<LANG=\"${LANG}\">" > "$output_contextes"
+
 for FICHIER in "$DOSSIER_DUMPS"/dump_"${LANG}"*.html; 
 do
     if [ -f "$FICHIER" ]; then
-
-		page=$(basename $FICHIER.txt)
+		page=$(basename $FICHIER| sed "s/dump_//;s/\.html$//")
         contenu=$(cat $FICHIER | tr -d '&<>')
         echo "<page=\"${page}\">" >> "$output_dump"
         echo "<text>${contenu}</text>" >> "$output_dump"
@@ -23,3 +26,15 @@ do
     fi
 done
 echo "</LANG>" >> "$output_dump"
+
+for FICHIER in "$DOSSIER_CONTEXTES"/contexte_"${LANG}"*.txt; 
+do
+    if [ -f "$FICHIER" ]; then
+		page=$(basename $FICHIER| sed "s/contexte_//;s/\.txt$//")
+        contenu=$(cat "$FICHIER" | tr -d '&<>')
+        echo "<page=\"${page}\">" >> "$output_contextes"
+        echo "<text>${contenu}</text>" >> "$output_contextes"
+        echo "</page> §" >> "$output_contextes"
+    fi
+done
+echo "</LANG>" >> "$output_contextes"
