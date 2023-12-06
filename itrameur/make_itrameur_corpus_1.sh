b24f9eb28359d05761928fb9d858cbbbd108d7e7
mariahul1 <hul.maria88@gmail.com>
2023-12-06 09:42:58 +0100

    Update script

--- itrameur/make_itrameur_corpus_1.sh
+++ itrameur/make_itrameur_corpus_1.sh
@@ -9,20 +9,23 @@ DOSSIER_DUMPS=$1
 DOSSIER_CONTEXTES=$2
 LANG=$3
 
-output_dump="../dumps-text/dump_${LANG}.txt"
+output_dump="PPE_lien/itrameur/dumps-text_${LANG}.txt"
 echo "<LANG=\"${LANG}\">" > "$output_dump"
 
-output_contextes="../contextes/contexte_${LANG}.txt"
+output_contextes="PPE_lien/itrameur/contextes_${LANG}.txt"
 echo "<LANG=\"${LANG}\">" > "$output_contextes"
 
 for FICHIER in "$DOSSIER_DUMPS"/dump_"${LANG}"*.html; 
 do
     if [ -f "$FICHIER" ]; then
 		page=$(basename $FICHIER| sed "s/dump_//;s/\.html$//")
-        contenu=$(cat $FICHIER | tr -d '&<>')
+        contenu=$(cat $FICHIER | tr -d '&<>' | sed -e 's/&/&amp;/g' -e 's/</&lt;/g' -e 's/>/&gt;/g')
         echo "<page=\"${page}\">" >> "$output_dump"
         echo "<text>${contenu}</text>" >> "$output_dump"
         echo "</page> §" >> "$output_dump"
+        #cleaned = $( echo $contenu | sed 's/&/&amp;/g')
+        #cleaned = $( echo $contenu | sed 's/</&lt;/g')
+        #cleaned = $( echo $contenu | sed 's/>/&gt;/g')
     fi
 done
 echo "</LANG>" >> "$output_dump"
@@ -31,7 +34,7 @@ for FICHIER in "$DOSSIER_CONTEXTES"/contexte_"${LANG}"*.txt;
 do
     if [ -f "$FICHIER" ]; then
 		page=$(basename $FICHIER| sed "s/contexte_//;s/\.txt$//")
-        contenu=$(cat "$FICHIER" | tr -d '&<>')
+        contenu=$(cat "$FICHIER" | tr -d '&<>'| sed -e 's/&/&amp;/g' -e 's/</&lt;/g' -e 's/>/&gt;/g')
         echo "<page=\"${page}\">" >> "$output_contextes"
         echo "<text>${contenu}</text>" >> "$output_contextes"
         echo "</page> §" >> "$output_contextes"
