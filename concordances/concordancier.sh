166b7bbf1e90839d41d48fc536848f4ae44ca6c9
Clara Sachot <raclax@Ordi.tout.court>
2023-12-12 19:27:04 +0100

    concordancier

--- concordances/concordancier.sh
+++ concordances/concordancier.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
 LANGUE=$1
-FICHIER=$2 
-CHEMIN="../contextes/contexte_$FICHIER.txt"
+FICHIER=$2
+CHEMIN=../contextes/contexte_$FICHIER.txt
 
-if $LANGUE '==' "ang"
+if $LANGUE == "ang"
 then
-	REGEXP="\blinks?\b"
+	REGEXP=\blinks?\b
 fi
 
-if $LANGUE '==' "pl"
+if $LANGUE == "pl"
 then
 	REGEXP="\b(Z|z)wiaz(ek|k(u|owi|iem|i|ow|om|ami|ach))\b"
 fi
 
-if $LANGUE '==' "kor"
+if $LANGUE == "kor"
 then
 	REGEXP=""
 fi
@@ -22,28 +22,26 @@ echo "
 <!DOCTYPE html>
 <html lang=\"${LANGUE}\">
 <head>
-<meta charset=\"UTF-8\">
-<title>Concordance</title>
+	<meta charset=\"UTF-8\">
+	<title>Concordance</title>
 </head>
 <body>
-<table>
-<thead>
-<tr>
-    <th class="has-text-right">Contexte gauche</th>
-    <th>Cible</th>
-    <th class="has-text-left">Contexte droit</th>
-</tr>
-</thead>
-<tbody>
-"
+	<table>
+	<thead>
+		<tr>
+    			<th class="has-text-right">Contexte gauche</th>
+    			<th>Mot</th>
+    			<th class="has-text-left">Contexte droit</th>
+		</tr>
+	</thead>
+	<tbody>"
+	
+grep -o -E -i "(\w+\W){0,5}\blinks?\b(\W+\w+){0,5}" "$CHEMIN" | sed -E 's/(.*)(\blinks?\b)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/'  
 
-
-grep -o -E -i "(\w+\W){0,5}($REGEXP)(\W+\w+){0,5}" "$CHEMIN" | sed -E "s/(.*)($REGEXP)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/g"  
- 
 
 echo "
-</tbody>
-</table>
+	</tbody>
+	</table>
 </body>
 </html>
 "
