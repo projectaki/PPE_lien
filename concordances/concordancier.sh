3e721c2d929647b126fe7ffe1ed4b9d5258f5fa3
Clara Sachot <raclax@Ordi.tout.court>
2023-12-12 14:54:22 +0100

    le concordancier encore

--- concordances/concordancier.sh
+++ concordances/concordancier.sh
@@ -17,20 +17,31 @@ then
 	$REGEXP=""
 fi
 
-echo "	<html>
-	<head>
-		<meta charset= \"UTF-8\">
-	</head>
-	<body>
-	<table>
-	<thead>
-	<tr><th>Contexte droit</th><th>Mot</th><th>Contexte gauche</th></tr>
-	</thead>
-	<tbody>"
+echo "
+<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+<meta charset=\"UTF-8\">
+<title>Concordance</title>
+</head>
+<body>
+<table>
+<thead>
+<tr>
+    <th class=\"has-text-right\">Contexte gauche</th>
+    <th>Cible</th>
+    <th class=\"has-text-left\">Contexte droit</th>
+</tr>
+</thead>
+<tbody>
+"
 
-grep -o -E -i "(\w+\W){0,5}$REGEXP(\W+\w+){0,5}" $PATH | sed -E "s/(.*)($REGEXP)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/g" > concordancier_$LANGUE.html
 
-echo"</tbody>
+grep -o -E -i "(\w+\W){0,5}$REGEXP(\W+\w+){0,5}" $PATH | sed -E "s/(.*)($REGEXP)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/" 
+
+echo "
+</tbody>
 </table>
-	</body>
-</html>" 
+</body>
+</html>
+"
