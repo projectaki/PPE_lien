1d36fb0193c47720f84dac1b34e253ae48c46556
Clara Sachot <raclax@Ordi.tout.court>
2023-12-12 14:45:38 +0100

    concordancier

--- concordances/concordancier.sh
+++ concordances/concordancier.sh
@@ -1,8 +1,36 @@
-TEXTE=$(grep -o -P -i "(\w+\W){0,5}links?(\W+\w+){0,5}" ./contextes/contexte_ang17.txt | sed 's/\(links\?\)/\t\0\t/g')
-while read -r TEXTE ;
-do
-	DEBUT=$(grep "^(*\t)" $TEXTE)
-	MOT=$(grep "\t*\t" $TEXTE)
-	FIN=$(grep "(\t*)$" $TEXTE)
-	echo $DEBUT\t$MOT\t$FIN
-done	
+LANGUE=$1
+
+$PATH = "../contextes/contexte_$LANGUE*.txt"
+
+if $LANGUE == "ang"
+then
+	$REGEXP="\blinks?\b"
+fi
+
+if $LANGUE == "pol"
+then
+	$REGEXP=""
+fi
+
+if $LANGUE == "kor"
+then
+	$REGEXP=""
+fi
+
+echo "	<html>
+	<head>
+		<meta charset= \"UTF-8\">
+	</head>
+	<body>
+	<table>
+	<thead>
+	<tr><th>Contexte droit</th><th>Mot</th><th>Contexte gauche</th></tr>
+	</thead>
+	<tbody>"
+
+grep -o -E -i "(\w+\W){0,5}$REGEXP(\W+\w+){0,5}" $PATH | sed -E "s/(.*)($REGEXP)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/g" > concordancier_$LANGUE.html
+
+echo"</tbody>
+</table>
+	</body>
+</html>" 
