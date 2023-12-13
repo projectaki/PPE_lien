678b5c0071aeae1218d8e42b2a6af1341a4e1bf2
mariahul1 <hul.maria88@gmail.com>
2023-12-13 11:05:39 +0100

    Update tableau pol

--- programmes/projet_pol.sh
+++ programmes/projet_pol.sh
@@ -23,14 +23,22 @@ echo "<html>
 	<body>"
 
 echo "		<table>
-		<tr><th>Numéro</th><th>URL</th><th>Code HTTP</th><th>Encodage</th><th>Aspiration</th><Dump</th><th>Nombre d'occurences</th><th>Contexte</th></tr>"
+		<tr><th>Numéro</th><th>URL</th><th>Code HTTP</th><th>Encodage</th><th>Aspiration</th><th>Dump</th><th>Nombre d'occurences</th><th>Contexte</th></tr>"
 while read -r URL
 do
-   	response=$(curl -s -I -L -w "%{http_code}" -o "./aspirations/aspiration_pl$N.html" $URL)
-	CODE=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | egrep -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
-    lynx -dump "$URL" > ./dumps-text/dump_pl${N}.html
-    COMPTE=$(cat ./dumps-text/dump_pl$N.html | grep -i -o -E "(Z|z)wiaz(ek|k(u|owi|iem|i|ow|om|ami|ach))"  | wc -w)
-    CONTEXTES=$(cat ./dumps-text/dump_pl$N.html | grep -B 1 -A 1 -i -w -E "(Z|z)wiaz(ek|k(u|owi|iem|i|ow|om|ami|ach))" > "./contextes/contexte_pl$N.txt" )
+   	response=$(curl -s -L -w "%{http_code}" -o "./aspirations/aspiration_pl$N.html" $URL)
+	CODE=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | egrep -o "charset=\S+" | cut -d"=" -f2 | tail -n 1 | tr '[:lower:]' '[:upper:]' )
+	if [ $response == 200 ]
+	then
+		if [ ! $CODE == "UTF-8" ]
+		then 
+			iconv -f "$CODE" -t "UTF-8" -o  "tmp/recode_${lineno}.html" "./aspirations/aspiration_pl$N.html"
+			mv "tmp/recode_${lineno}.html" > "./aspirations/aspiration_pl$N.html"
+		fi
+    lynx -assume_charset UTF-8 -dump -nolist "$URL" > ./dumps-text/dump_pl${N}.html
+    COMPTE=$(cat ./dumps-text/dump_pl$N.html | grep -i -o -E "zwiaz(ek|k(u|owi|iem|i|ow|om|ami|ach))"  | wc -w)
+    cat ./dumps-text/dump_pl$N.html | grep -C 3 -i -E "zwiaz(ek|k(u|owi|iem|i|ow|om|ami|ach))" > "./contextes/contexte_pl$N.txt" 
+	fi
 
     echo "<tr>
     <td>$N</td>
@@ -41,7 +49,7 @@ do
     <td><a href='../dumps-text/dump_pl$N.html'>dump</a></td>
     <td>$COMPTE</td>
     <td><a href='../contextes/contexte_pl$N.txt'>contexte</a></td>
-    </tr>" 
+    </tr>"
     N=$((N + 1))
 
 done < "$URL"
