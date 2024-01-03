6c092f5cf0b2d2be15c66acdb4affea1d0a3e3e1
Débora <vdzdebora@gmail.com>
2024-01-03 22:33:10 +0100

    Modifications coréen

--- programmes/projet_kor.sh
+++ programmes/projet_kor.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 urls=$1
+N=0
 
 if [ $# -ne 1 ]
 then
@@ -7,7 +8,7 @@ then
 Usage : $0 \"../URLs/liens_gwangye.txt\""
 	exit
 else
-	if [ -f "${urls}" ]
+	if [ -f "$urls" ]
 	then 
 		echo " "
 	else
@@ -30,44 +31,42 @@ echo "  <table>
 			<th>encodage HTTP</th>
 			<th>Encodage/th>
 			<th>Aspiration</th>
-			<Dump</th>
+			<th>Dump</th>
 			<th>Nombre d'occurences</th>
 			<th>Contexte</th></tr>"
 
 while read -r urls;
 do
-N=0
-LANG="kor"
-reponse=$(curl -s -I -L -w "%{http_encodage}" -o "../aspirations/aspiration_${LANG}${N}.html" $urls)
-encodage=$(curl -s -I -L -w "%{content_type}" -o /dev/null $urls | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1 | tr '[:lower:]' '[:upper:]')
+reponse=$(curl -s -L -w "%{http_code}" -o "../aspirations/aspiration_kor$N.html" $urls)
+encodage=$(curl -s -I -L -w "%{content_type}" -o /dev/null $urls | grep -o "charset=\S+" | cut -d"=" -f2 | tail -n 1 | tr '[:lower:]' '[:upper:]')
 
-if [ $reponse -eq 200 ]
+if [ $reponse == 200 ]
 then 
 	if [ ! $encodage == "UTF-8" ]
 	then 
-		iconv -f "$encodage" -t "UTF-8" -o "/tmp/reencodage_${N}.html" "./aspirations/${LANG}-${N}.html"
-		mv "/tmp/reencodage_${N}.html" "./aspirations/${LANG}-${N}.html"
+		iconv -f "$encodage" -t "UTF-8" -o "/tmp/reencodage_${lineno}.html" "./aspirations/kor$N.html"
+		mv "/tmp/reencodage_${lineno}.html" "./aspirations/kor$N.html"
 	fi	
 
-	lynx -assume_charset UTF-8 -dump -nolist ./apsirations/${LANG}-${N}.html > "../dumps-text/dump_${LANG}${N}.txt"
-	compte=$(cat "../dumps-text/dump_${LANG}${N}.html" | egrep -i -o "관계"  | wc -w)
-    cat "../dumps-text/dump_${LANG}${N}.html" | egrep -2 "관계" > "../contextes/contexte_${LANG}${N}.txt"
-	echo "<tr>
-		<td>${N}</td>
-		<td>${urls}</td>
-		<td>${reponse}</td>
-		<td>${encodage}</td>
-		<td><a href="../aspirations/aspiration_${LANG}${N}.html">Aspiration</a></td>
-        <td><a href="../dumps-text/dump_${LANG}${N}.html">Dump</a></td>
-        <td>${compte}</td>
-        <td><a href="../contextes/contexte_${LANG}${N}.txt">Contexte</a></td>
-	</tr>"
-	N=$(expr $N + 1)
+	lynx -assume_charset UTF-8 -dump -nolist "$urls" > "../dumps-text/dump_kor$N.txt"
+	compte=$(cat "../dumps-text/dump_kor$N.html" | egrep -w "관계" | wc -w)
+    cat "../dumps-text/dump_kor$N.html" | egrep "관계" > "../contextes/contexte_kor$N.txt"
 fi
+echo "<tr>
+	<td>$N</td>
+	<td><a href="$urls">$urls</a></td>
+	<td>$reponse</td>
+	<td>$encodage</td>
+	<td><a href="../aspirations/aspiration_kor$N.html">Aspiration</a></td>
+    <td><a href="../dumps-text/dump_kor$N.html">Dump</a></td>
+    <td>$compte</td>
+    <td><a href="../contextes/contexte_kor$N.txt">Contexte</a></td>
+	</tr>"
+	N=$((N + 1))
 
 done < "$urls"
 echo "		</table>
 	</body>
 </html>" 
 
-# bash projet_${LANG}.sh ../URLs/liens_gwangye.txt > "../tableaux/tableau_${LANG}.html"
+# bash projet_kor.sh ../URLs/liens_gwangye.txt > "../tableaux/tableau_kor.html"
